4c0fe56abd2f96fe86ca7674045971523b291327
ViZacatzi <vizacatzi@gmail.com>
2023-12-04 10:40:37 -0600

    vista API y cambios de vista error

--- src/views/apikey.js
+++ src/views/apikey.js
@@ -1,5 +1,20 @@
 export const ApiKey = () => {
-    const p = document.createElement("p");
-    p.innerHTML = "Hola este la vista API";
-    return p;
-  };
+  const elementoApi = document.createElement("div");
+
+  // Agregar el contenido al elemento header
+  elementoApi.innerHTML = `
+                          <div class="contenedor-api">
+                            <p class="api">API-KEY</p><br>
+                            <label class="texto" for="user">Usuario:</label>
+                            <input type="text" id="user"><br>
+                            <label class="texto" for="pass">Contraseña:</label>
+                            <input type="password" id="pass"><br>
+                              <div class="botones"><br>
+                               <button class="boton-api">Aceptar</button>
+                               <button class="boton-limpiar">Borrar todo</button>
+                              </div>
+                          </div>
+  `;
+
+  return elementoApi;
+};
