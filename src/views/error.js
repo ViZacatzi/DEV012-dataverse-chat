672f433109d5f2b1adeba105113af487ef43e936
hem-rodriguez <hem.rodriguez@hotmail.com>
2023-12-04 10:05:19 -0600

    agregamos los filtros en la carpeta de home

--- src/views/error.js
+++ src/views/error.js
@@ -1,14 +1,15 @@
+// error.js
+
 export const vistaError = () => {
-  
   const elementoError = document.createElement('div');
 
-  // Agregar el contenido al elemento header
-  elementoError.innerHTML = `<div class="contenedor"> <div class="error">ERROR 404</div>
-                             <button class="boton-regresar">Click aquí para regresar</button></div>`;
-                             
+  // Agregar el contenido al elemento de error
+  elementoError.innerHTML = `<div class="contenedor"> 
+                               <div class="error">ERROR 404
+                                 <button class="boton-regresar">Click aquí para regresar</button>
+                               </div>
+                             </div>`;
 
   return elementoError;
-
-
 };
- 
+
