89ee9b61adc476b5862ce74f426fb4efc4edd3bf
ViZacatzi <vizacatzi@gmail.com>
2023-12-06 23:10:13 -0600

    funciones de filtrado y ordenamiento pintadas y con funcionalidad

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import { PanelChat } from "./views/panel.js";
 import { ApiKey } from "./views/apikey.js";
 import { vistaError } from "./views/error.js";
 import { onURLChange, setRootElement, setRouters } from "./router.js";
-import { calcularEstadistica } from "./Lib/datafunctions.js";
-import { filterMoviesByGenre } from "./Lib/datafunctions.js";
-import { sortedMovies } from "./Lib/datafunctions.js";
 
 
 const routes = {
@@ -32,54 +29,7 @@ onURLChange(event.target.location.pathname)
   //    alert("Hola");
   //  });
   
-  const selectFiltros = document.querySelector("#filtros"); // Selector para filtros
-  const selectOrdenamiento = document.querySelector("#ordenamiento");
-  const buttonBorrar = document.querySelector('[data-testid="button-clear"]'); //selector de borrar
-  
-  //evento de cambio para selecctor de fitros
-  
-  selectFiltros.addEventListener("change", (evento) => {
-    //console.log;
-   tarjetas.innerHTML = ""; //limpiar el contenido del contenedor prin
-   const selectedGenre = selectFiltros.value; // Obtener el género seleccionado del selector de filtros
-   const filteredMovies = filterMoviesByGenre(data, selectedGenre); // Usa la función de filtrado
-    // Filtrar las películas por el género seleccionado
-   tarjetas.appendChild(renderItems(filteredMovies));
-    // Renderizar y agregar las películas filtradas al contenedor principal
-    
-  });
-  console.log(selectFiltros)
-
-  // Evento de cambio para el selector de ordenamiento
-  selectOrdenamiento.addEventListener("change", () => {
-    tarjetas.innerHTML = "";
-    // Limpiar el contenido del contenedor principal
-    const selectedGenre = selectFiltros.value;
-    // Obtener el género seleccionado del selector de filtros
-    const dataSort = sortedMovies(data, selectedGenre, selectOrdenamiento.value);
-    // Utiliza la función sortedMovies para ordenar las películas filtradas
-    tarjetas.appendChild(renderItems(dataSort));
-    // Renderizar y agregar las películas ordenadas al contenedor principal
-  });
-  
-  buttonBorrar.addEventListener("click", () => {
-    // Evento de clic para el botón de borrado
-    tarjetas.innerHTML = "";
-    // Limpiar el contenido del contenedor principal
-    tarjetas.appendChild(renderItems(data));
-    // Renderizar y agregar todas las películas al contenedor principal
-  });
-  
-  // El código para calcular la estadística de películas de terror ya se encuentra en dataFunctions.js
-  const generoTerror = "Terror";
-  const totalPeliculasDeTerror = calcularEstadistica(data, generoTerror);
-  // Calcular estadísticas para el género 'Terror'
-  const estadisticaElement = document.getElementById("peliculasDeTerror");
-  // está obteniendo el elemento del DOM con el identificador 'peliculasDeTerror' y
-  //asignándolo a la variable estadisticaElement.
-  estadisticaElement.textContent = totalPeliculasDeTerror.toString();
-  //  está actualizando el contenido del elemento de estadística en el DOM
-  //con el valor total de películas de terror, que se ha convertido a una cadena de texto.
+ 
   
 
 });
