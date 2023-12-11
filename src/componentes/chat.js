f13b8ffaa3b11003b4fe65dfd4563672f358b0e5
ViZacatzi <vizacatzi@gmail.com>
2023-12-11 09:57:47 -0600

    Merge de la funcion de las tarjetas y CSS de vista panel

--- src/componentes/chat.js
+++ src/componentes/chat.js
@@ -4,19 +4,18 @@ export const chat = () => {
 
   // Agregar el contenido al elemento header
   chatElement.innerHTML = `
-                          <div>
+                          
                            <h4 class="texto">Chatea con...</h4><br>
-                          </div>
-                          <div class="contenedor-vista-chat">
-                            <div class="caja-chat"></div>
-                            <div class="contenedor-chat">
+                          
+                          
+                            <input class="caja-chat"></input>
+                            
                               <textarea for="enviar" class="texto-chat"></textarea><br>
                               <button>Enviar</button>
-                            </div>
-                            <div class="contenedor-boton-regreso">
+                            
+                            
                               <button class="boton-chat">Regresar</button>
-                            </div>
-                          </div>
+                            
 `;
   return chatElement;
 };
