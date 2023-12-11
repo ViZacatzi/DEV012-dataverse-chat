99d77256f4a9f5862cc7b4b72e2fad5c21cdf58b
ViZacatzi <vizacatzi@gmail.com>
2023-12-11 13:53:43 -0600

    función find en la vista de personaje y cambios en CSS de la vista panel

--- src/componentes/chat.js
+++ src/componentes/chat.js
@@ -3,19 +3,12 @@ export const chat = () => {
   const chatElement = document.createElement('div');
 
   // Agregar el contenido al elemento header
-  chatElement.innerHTML = `
-                          
+  chatElement.innerHTML = `       
                            <h4 class="texto">Chatea con...</h4><br>
-                          
-                          
-                            <input class="caja-chat"></input>
-                            
-                              <textarea for="enviar" class="texto-chat"></textarea><br>
-                              <button>Enviar</button>
-                            
-                            
-                              <button class="boton-chat">Regresar</button>
-                            
+                            <div class="caja-chat"></div>
+                            <textarea for="enviar" class="texto-chat"></textarea><br>
+                            <button>Enviar</button>
+                            <button class="boton-chat">Regresar</button>                        
 `;
   return chatElement;
 };
