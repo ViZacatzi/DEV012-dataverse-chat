ae4ec9f30c2b5b46cfaf2fb95560a50e3054c96a
hem-rodriguez <hem.rodriguez@hotmail.com>
2023-12-19 11:46:24 -0600

    cambios en css del chat en carpeta de pelicula y modificacion en la data para ver la pelicula en la vista

--- src/views/panel.js
+++ src/views/panel.js
@@ -3,15 +3,15 @@ import { chat } from "../componentes/chat.js";
 
 export const PanelChat = () => {
   
-  const divPanel = document.createElement("div");
-  divPanel.id = ("contenedor-panel")
-  const panelView = divPanel;
+  //const divPanel = document.createElement("div");
+  //divPanel.id = ("contenedor-panel")
+  //const panelView = divPanel;
 
-  const chatComponent = chat();
-  panelView.appendChild(chatComponent);
+  //const chatComponent = chat();
+  //panelView.appendChild(chatComponent);
 
-  const chatFooter = footer();
-  panelView.appendChild(chatFooter);
+  //const chatFooter = footer();
+  //panelView.appendChild(chatFooter);
 
-  return panelView;
+  //return panelView;
 };
--- src/views/pelicula.js
+++ src/views/pelicula.js
@@ -1,37 +1,35 @@
 import { footer } from "../componentes/footer.js";
 import data from "../data/dataset.js";
+import { chat } from "../componentes/chat.js";
 
 export const pelicula = (movieName) => {
-  
-    const peliculaEncontrada = data.find((content) => content.name === movieName);
+  const peliculaEncontrada = data.find((content) => content.name === movieName);
 
-    if (peliculaEncontrada) {
-      const contenidoPelicula = document.createElement("div");
-      contenidoPelicula.classList.add("tarjeta-pelicula");
-      contenidoPelicula.setAttribute("itemtype", "peliculas");
-      contenidoPelicula.innerHTML = ` 
+  if (peliculaEncontrada) {
+    const contenidoPelicula = document.createElement("div");
+    contenidoPelicula.classList.add("tarjeta-pelicula");
+    contenidoPelicula.setAttribute("itemtype", "peliculas");
+    contenidoPelicula.innerHTML = ` 
               <img class="imagen-pelicula" src="${peliculaEncontrada.imageUrl}">
-              <dt>Nombre:</dt><dd itemprop="name">${peliculaEncontrada.name}</dd>
+              <div> <dt>Nombre:</dt><dd itemprop="name">${peliculaEncontrada.name}</dd>
               <dt>description:</dt><dd itemprop="description">${peliculaEncontrada.description}</dd>
               <dt>genre:</dt><dd itemprop="genre">${peliculaEncontrada.genre}</dd>
               <dt>yearOfRelease:</dt><dd itemprop="yearOfRelease">${peliculaEncontrada.facts.yearOfRelease}</dd>
               <dt>director:</dt><dd itemprop="director">${peliculaEncontrada.facts.director}</dd>
               <dt>mainActors:</dt><dd itemprop="mainActors">${peliculaEncontrada.facts.mainActors}</dd>
+              <button id="abrirChat">Abrir Chat</button>
+              <div id="contenedorChat"></div>
         `;
-           
+    const contenedorChat = contenidoPelicula.querySelector("#contenedorChat");
+    const botonAbrirChat = contenidoPelicula.querySelector("#abrirChat");
 
-      return contenidoPelicula;
-    } else {
-      return "Pelicula no encontrada";
-    }
-  };
+    botonAbrirChat.addEventListener("click", () => {
+      contenedorChat.innerHTML = ""; // Limpia el contenedor antes de agregar la vista
+      contenedorChat.appendChild(chat());
+    });
 
-  
-
-
-
-
-
-
- 
-
+    return contenidoPelicula;
+  } else {
+    return "Pelicula no encontrada";
+  }
+};
